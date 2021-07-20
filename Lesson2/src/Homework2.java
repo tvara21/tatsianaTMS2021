ae8dc7a09f3cea86c3ad6d7625531c695c0f6111
tvara21 <86020244+tvara21@users.noreply.github.com>
2021-07-20 16:21:18 +0200

    Delete Homework2.java

--- Lesson2/src/Homework2.java
+++
@@ -1,8 +0,0 @@
-//Задача 2: Найти максимальное значение из двух чисел
-public class Homework2 {
-    public static void main(String[] args) {
-        int a = 1256;
-        int b = 5889;
-        System.out.println("максимальное значение: " + Math.max(a, b));
-    }
-}
