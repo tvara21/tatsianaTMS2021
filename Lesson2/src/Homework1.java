d9b896bedb007eb55ed5b5707a2d2850e6dbaf6f
tvara21 <86020244+tvara21@users.noreply.github.com>
2021-07-20 16:33:40 +0200

    Delete Lesson2/src directory

--- Lesson2/src/Homework1.java
+++
@@ -1,8 +0,0 @@
-//Задача 1: Найти сумму двух чисел a и b
-public class Homework1 {
-    public static void main(String[] args) {
-        int a = 23;
-        double b = 0.368;
-        System.out.println("сумма: " + a + b);
-    }
-}
